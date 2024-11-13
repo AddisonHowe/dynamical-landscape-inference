fb39c9530c702075b5f60e0a7eb9c7cbb0dece4b
Addison Howe <addi.howe@gmail.com>
2024-11-13 14:50:15 -0600

    modified autofig scripts

--- scripting/autofig/alg_synbindec/generate_ai_files_from_template.sh
+++ scripting/autofig/alg_synbindec/generate_ai_files_from_template.sh
@@ -12,13 +12,17 @@ PROJ_DIR="${PROJ_DIR_TILDE/#\~/$HOME}"
 # Output filename suffix, to append to the model name
 IMAGE_SUFFIX=image1
 
-sleeptime=5
+sleeptime=3
 
 # Template .ai file, and the folder containing images linked in the template.
 # Note that the folder name will be replaced, and therefore needs to use the
 # tilde explicitly, without substitution, for the filename.
 template_fpath=$PROJ_DIR/scripting/autofig/alg_synbindec/alg_synbindec_template1.ai
 template_linkdir=$PROJ_DIR_TILDE/scripting/autofig/alg_synbindec/template1_images
+
+# Log file
+logfpath=$PROJ_DIR/scripting/logs/log_alg_synbindec.txt
+echo "Log" > $logfpath
 
 # This is where all generated ai files will be stored, one for every run below.
 aioutdir=${PROJ_DIR}/figures/autofig/out/alg_synbindec
@@ -38,13 +42,15 @@ rundirs=$(ls $RUNDIRBASE)
 for modelname in ${rundirs[@]}; do
     rd=$PROJ_DIR/$RUNDIRBASE/$modelname
     echo $modelname
+    echo $modelname >> $logfpath
     fname=${modelname}_${IMAGE_SUFFIX}
     cp $template_fpath $aioutdir/$fname.ai
-    open -a "$ILLUSTRATOR_PATH" $aioutdir/$fname.ai
+    open -g -a "$ILLUSTRATOR_PATH" $aioutdir/$fname.ai
     sleep $sleeptime
     sed -e "s|<OLD_FOLDER_PATH>|$template_linkdir|" \
+        -e "s|<LOGFPATH>|$logfpath|" \
         -e "s|<NEW_FOLDER_PATH>|$rd|" $scriptfpath > $tmp_script_fpath
-    osascript -e 'tell application "Adobe Illustrator" to do javascript file "'"$tmp_script_fpath"'"';
+    osascript -e 'tell application "Adobe Illustrator" to do javascript file "'"$tmp_script_fpath"'"' && >> $logfpath;
     rm $tmp_script_fpath
     rm $aioutdir/$fname.ai  # remove .ai file, keeping only the pdf
 done
--- scripting/autofig/modify_links.jsx
+++ scripting/autofig/modify_links.jsx
@@ -6,23 +6,32 @@ app.userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
 var oldFolder = "<OLD_FOLDER_PATH>";
 var newFolder = "<NEW_FOLDER_PATH>";
+var logfpath = "<LOGFPATH>";
 
 var doc = app.activeDocument;
 var links = doc.placedItems;
 
 var idxsToRemove = [];  // Track the indexes of the links to remove.
 
+var numReplaced = 0;
+var numRemoved = 0;
 for (var i = 0; i < links.length; i++) {
     var link = links[i];
-    if (link.file && link.file.fullName.indexOf(oldFolder) !== -1) {
-        var newFilePath = link.file.fullName.replace(oldFolder, newFolder);
-        var newFile = new File(newFilePath);
-        if (newFile.exists) {
-            link.file = newFile;
-        } else {
-            // Wait to remove the link so as to not mess of the loop
-            idxsToRemove.push(i)
+    try {
+        if (link.file && link.file.fullName.indexOf(oldFolder) !== -1) {
+            var newFilePath = link.file.fullName.replace(oldFolder, newFolder);
+            var newFile = new File(newFilePath);
+            if (newFile.exists) {
+                link.file = newFile;
+                numReplaced++;
+            } else {
+                // Wait to remove the link so as to not mess up the loop
+                idxsToRemove.push(i)
+                numRemoved++;
+            }
         }
+    } catch (e) {
+        alert("Caught Error:" + e)
     }
 }
 
@@ -43,4 +52,23 @@ var PDFopts = new PDFSaveOptions();
 PDFopts.pDFPreset = "[Illustrator Default]";
 doc.saveAs(pdfFile, PDFopts);
 
+// Log message
+var msg = "  Summary:\n\tReplacement folder: " + newFolder + "\n" + 
+            "\tTotal number of links: " + nlinks + "\n" +
+            "\tReplaced: " + numReplaced + "\n" +
+            "\tFailed to replace: " + numRemoved + "\n";
+writeToFile(msg, logfpath);
+
 doc.close();
+
+
+function writeToFile(msg, fpath) {
+    try {
+        var log = File(fpath);
+        log.open('a');
+        log.write(msg + '\n');
+        log.close()
+    } catch (e) {
+        $.writeln (e);
+    }
+};
